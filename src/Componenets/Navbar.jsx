be4143c8201fcce7bd2da135504997cf125fecef
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-14 23:40:10 +0100

    Update navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' mx-4 p-3 h-20 '>
+        <div className=' flex item-center justify-center  bg-green-400 w-50 h-10 '>
+
+        </div>
+
+
+    </div>
   )
 }
