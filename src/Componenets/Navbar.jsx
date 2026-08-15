ff5a4c3faa01efef7b3ff452a718d52b74ce94ad
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-15 23:54:29 +0100

    update navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="p-2 h-20 w-full bg-red-400 flex items-center justify-center ">
-      <div className=" flexss item-center justify-center w-100 border-none border-2 border-b-gray-400 outline-none  bg-green-400 w-50 h-10 justify-between items-center flex p-2 rounded-md ">
+    <div className="flex justify-between items-center w-100 border-none border-2 border-b-gray-400 outline-none  bg-green-400 w-50 h-10 justify-between items-center flex p-2 rounded-md ">
+      
         <div>
-          <h4> Sneakers</h4>
+          <h4 className="text-black text-lg font-bold">Sneakers</h4>
 
           <div className="flex gap-4 ">
-            {" "}
-            <a href=""> Collection</a>
-            <a href="">Men</a>
-            <a href="">Women</a>
+            
+            <a href="" className="text-white hover:text-gray-300">Collection</a>
+            <a href="" className="text-white hover:text-gray-300">Men</a>
+            <a href="" className="text-white hover:text-gray-300">Women</a>
           </div>
         </div>
 
         <div></div>
-      </div>
+  
     </div>
   );
 };
