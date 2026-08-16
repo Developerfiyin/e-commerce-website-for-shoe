ee54baf47cbbb43be5f27e50117982978dd26428
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-16 23:43:31 +0100

    update navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -1,11 +1,9 @@
-import React from "react"
+import React from "react";
 import image from "../assets/icon-cart.svg";
-import image2 from '../assets/image-avatar.png';
+import image2 from "../assets/image-avatar.png";
 export const Navbar = () => {
   return (
-    <div className="flex justify-between items-center w-300 border-none border-2 border-b-gray-400 outline-none  bg-green-400 p-2 rounded-md ">
-  
-                        
+    <div className="flex justify-between mx-auto items-center w-300 border-none border-2 border-b-gray-400 outline-none  bg-green-400 p-2 rounded-md ">
       <div className="flex gap-4 items-center">
         <h4 className="text-black text-lg font-bold">Sneakers</h4>
 
@@ -23,9 +21,8 @@ export const Navbar = () => {
       </div>
 
       <div className="flex gap-4 items-center">
-   <img src={image} alt="Cart icon" className="w-6 h-6 rounded-full  " />
-    <img src={image2} alt="Cart"  className="w-8 h-8 rounded-full" />
-
+        <img src={image} alt="Cart icon" className="w-6 h-6 rounded-full  " />
+        <img src={image2} alt="Cart" className="w-8 h-8 rounded-full" />
       </div>
     </div>
   );
