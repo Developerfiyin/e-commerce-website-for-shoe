1ac311f2a20db4df9e2b4b05f72961389df6f17a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-16 22:50:29 +0100

    update navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -2,21 +2,24 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="flex justify-between items-center w-100 border-none border-2 border-b-gray-400 outline-none  bg-green-400 w-50 h-10 justify-between items-center flex p-2 rounded-md ">
-      
-        <div>
-          <h4 className="text-black text-lg font-bold">Sneakers</h4>
+    <div className="flex justify-between items-center w-200 border-none border-2 border-b-gray-400 outline-none  bg-green-400 w-50 h-10 justify-between items-center flex p-2 rounded-md ">
+      <div className="flex gap-4 items-center">
+        <h4 className="text-black text-lg font-bold">Sneakers</h4>
 
-          <div className="flex gap-4 ">
-            
-            <a href="" className="text-white hover:text-gray-300">Collection</a>
-            <a href="" className="text-white hover:text-gray-300">Men</a>
-            <a href="" className="text-white hover:text-gray-300">Women</a>
-          </div>
+        <div className="flex gap-4 ">
+          <a href="" className="text-white hover:text-gray-300">
+            Collection
+          </a>
+          <a href="" className="text-white hover:text-gray-300">
+            Men
+          </a>
+          <a href="" className="text-white hover:text-gray-300">
+            Women
+          </a>
         </div>
+      </div>
 
-        <div></div>
-  
+      <div></div>
     </div>
   );
 };
