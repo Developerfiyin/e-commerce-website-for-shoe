90505350901e4e311aeae14847c5f8169b191f07
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-17 16:18:54 +0100

    Update navbar.jsx

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -3,26 +3,42 @@ import image from "../assets/icon-cart.svg";
 import image2 from "../assets/image-avatar.png";
 export const Navbar = () => {
   return (
-    <div className="flex justify-between mx-auto items-center border-x-0 border-t-0 border-0 border-2 border-b-gray-400 outline-none p-2 rounded-md ">
-      <div className="flex gap-8 items-center bg-red-400">
-        <h4 className="text-black text-2xl font-bold">Sneakers</h4>
+    <div className="flex justify-between mx-auto items-center pt-2 p-blue-300 border-x-0 border-t-0 border-0 border-2 border-b-gray-400 outline-none p-2 rounded-md ">
+      <div className="flex gap-8 items-center justify-center w-150">
+        <h4 className="text-black text-2xl font-bold text-hsl(220, 13%, 13%)">
+          Sneakers
+        </h4>
 
-        <div className="flex gap-6 items-center">
-          <a href="" className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500">
+        <div className="flex gap-6 items-center ">
+          <a
+            href=""
+            className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500"
+          >
             Collection
           </a>
-          <a href="" className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500">
+          <a
+            href=""
+            className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500"
+          >
             Men
           </a>
-          <a href="" className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500">
+          <a
+            href=""
+            className="text-gray-400 font-medium transition duration-300 cursor-pointer hover:text-gray-500"
+          >
             Women
           </a>
         </div>
       </div>
 
-      <div className="flex gap-4 items-center">
-        <img src={image} alt="Cart icon" className="w-6 h-6 rounded-full  " />
-        <img src={image2} alt=" Avatar image " className="w-8 h-8 border-2 rounded-full hover:border-green-500 " />
+      <div className="flex gap-6 items-center">
+        <img src={image} alt="Cart icon" className="w-6 h-6 rounded-full" />
+        <img
+          src={image2}
+          alt=" Avatar image "
+          className="w-10 h-10 hover:border-2 rounded-full hover:border-Orange:hsl(26, 100%, 55%)
+ "
+        />
       </div>
     </div>
   );
