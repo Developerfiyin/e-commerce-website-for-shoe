552ee20a5cc528b1f838cc3d8a951e371f472662
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-20 23:53:21 +0100

    optimized the page layout

--- src/Componenets/Header.jsx
+++ src/Componenets/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 import product1 from "../assets/image-product-1.jpg";
+import image from "../assets/icon-cart.svg";
 
 const Header = () => {
   return (
@@ -14,7 +15,7 @@ const Header = () => {
           {/*the other mail container */}
 
           <div className="w-1/2 flex flex-col gap-6">
-            <p className="text-gray font-bold "> SNEAKER COMPANY</p> 
+            <p className="text-gray font-bold "> SNEAKER COMPANY</p>
             <h3 className="text-3xl text-secondary font-bold  ">
               Fall Limited Edition Sneakers{"  "}
             </h3>
@@ -24,23 +25,36 @@ const Header = () => {
               everything the weather can offer.
             </p>
 
-            <div className=" flex gap-6 items-center">    
+            <div className=" flex gap-6 items-center">
               <h3 className="font-bold text-2xl "> $125.00</h3>
-              <h4 className=" font-semibold text-light-gray bg-secondary text-center py-0 px-2 rounded-md"> 50%</h4>
+              <h4 className=" font-semibold text-light-gray bg-secondary text-center py-0 px-2 rounded-md">
+                {" "}
+                50%
+              </h4>
             </div>
-            < p className="text-gray font-bold "> 250.00</p> 
-            <div className="flex gap-6 items-center bg-primary">
-              <div>
-              <button type="submit" className="bg-green-400" > 0</button>
-
+            <p className="text-gray font-bold "> $250.00</p>
+            <div className="flex gap-4 items-center bg-primary justify-evenly text-center  ">
+              <div className="flex gap-4 bg-pale rounded-lg w-40 items-center justify-center py-2 px-3  ">
+                <button className="text-primary font-semibold text-xl text-center  ">
+                  -
+                </button>
+                <p className="text-center">0 </p>
+                <button className="text-primary font-semibold text-xl text-center ">
+                  +
+                </button>
               </div>
-              <div>
-              <button type="submit" className="bg-secondary" > Add to cart</button>
+              <div className=" items-center w-60 gap-4 flex justify-center bg-pale py-2 px-3 rounded-lg ">
+                <img src={image} alt="" />
 
+                <button
+                  type="submit"
+                  className="bg-secondary text-center text-white"
+                >
+                  {" "}
+                  Add to cart
+                </button>
               </div>
             </div>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-
           </div>
         </div>
       </div>
